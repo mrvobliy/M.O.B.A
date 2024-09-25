cad9251816bb66fe2d3bb4a372b8e0c8dc82fd58
Daniil Gorin <daniil009.gorin@yandex.ru>
2024-09-25 13:52:25 +0300

    add player hit damage effect

---
+++ Assets/Game resources/Scripts/_Core/PlayerDamageEffect.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using com.cyborgAssets.inspectorButtonPro;
+using DG.Tweening;
+using UnityEngine;
+
+public class PlayerDamageEffect : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    
+    [SerializeField] private List<Material> _materials;
+    [SerializeField] private Color _startColor;
+    [SerializeField] private Color _damageColor;
+    [SerializeField] private float _timeChangeColor;
+
+    private void OnEnable()
+    {
+        _player.OnDamageTaken += ChangeColor;
+    }
+
+    private void OnDisable()
+    {
+        _player.OnDamageTaken -= ChangeColor;
+    }
+
+    [ProButton]
+    private void ChangeColor()
+    {
+        foreach (var mat in _materials)
+        {
+            mat.DOColor(_damageColor, _timeChangeColor).OnComplete(() =>
+            {
+                mat.DOColor(_startColor, _timeChangeColor);
+            });
+        }
+    }
+}
