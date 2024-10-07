b4facc20f4dfd38f3d9023ca307208027a6e5fb5
Daniil Gorin <daniil009.gorin@yandex.ru>
2024-10-07 14:04:46 +0300

    add enenmy rebound at player attacks

--- Assets/Game resources/Scripts/_Core/Attacker.cs
+++ Assets/Game resources/Scripts/_Core/Attacker.cs
@@ -6,7 +6,6 @@ using UnityEditor;
 public abstract class Attacker : Target
 {
 	[Header("Attacker")]
-	[SerializeField] protected Transform _rotationParent;
 	[SerializeField] private Projectile _projectilePrefab;
 	[SerializeField] private Transform _projectileOriginLeft;
 	[SerializeField] private Transform _projectileOriginRight;
