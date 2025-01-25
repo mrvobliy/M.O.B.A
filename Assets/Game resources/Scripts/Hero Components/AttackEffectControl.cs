6327c09ed67371186c6051c5f3bbeb1f00461ca7
Daniil Gorin <daniil009.gorin@yandex.ru>
2025-01-25 15:26:14 +0300

    fix player attack effects control

--- Assets/Game resources/Scripts/Hero Components/AttackEffectControl.cs
+++ Assets/Game resources/Scripts/Hero Components/AttackEffectControl.cs
@@ -4,41 +4,36 @@ using Random = UnityEngine.Random;
 
 public class AttackEffectControl : MonoBehaviour
 {
-    [SerializeField] private Attacker _attacker;
+    [SerializeField] private HeroAttackControl _heroAttackControl;
     [SerializeField] private AnimationEvents _animationEvents;
     [SerializeField] private List<ParticleSystem> _attackEffects;
     [SerializeField] private List<ParticleSystem> _hitEnemyEffects;
     [SerializeField] private Transform _spawnPoint;
-    [SerializeField] private bool _isRandomAttackEffect;
-    [SerializeField] private bool _isRandomEnenmyHitEffect;
     
     private void OnEnable()
     {
-        _attacker.OnTargetHit += PlayEnemyHitEffect;
+        _heroAttackControl.OnTargetsHit += PlayEnemyHitEffect;
         _animationEvents.OnPlayAttackEffect += PlayAttackEffect;
     }
 
     private void OnDisable()
     {
-        _attacker.OnTargetHit -= PlayEnemyHitEffect;
+        _heroAttackControl.OnTargetsHit -= PlayEnemyHitEffect;
         _animationEvents.OnPlayAttackEffect -= PlayAttackEffect;
     }
 
     private void PlayAttackEffect()
     {
-        if (_isRandomAttackEffect)
-        {
-            var index = Random.Range(0, _attackEffects.Count);
-            Instantiate(_attackEffects[index], _spawnPoint.position, _spawnPoint.rotation);
-        }
+        var index = Random.Range(0, _attackEffects.Count);
+        Instantiate(_attackEffects[index], _spawnPoint.position, _spawnPoint.rotation);
     }
     
-    private void PlayEnemyHitEffect(Target target)
+    private void PlayEnemyHitEffect(List<EntityComponentsData> enemies)
     {
-        if (_isRandomEnenmyHitEffect)
+        foreach (var enemy in enemies)
         {
             var index = Random.Range(0, _hitEnemyEffects.Count);
-            Instantiate(_hitEnemyEffects[index], target.GetAttackPoint().position, Quaternion.identity);
+            Instantiate(_hitEnemyEffects[index], enemy.EntityHealthControl.EnemyAttackPoint.position, Quaternion.identity);
         }
     }
 }
