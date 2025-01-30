168185ea539eba5a6f3a48e7f087132f48e1dfae
Daniil Gorin <daniil009.gorin@yandex.ru>
2025-01-30 13:08:02 +0300

    sorting files

--- Assets/Game resources/Scripts/UI/CanvasSafeAreaHelper.cs
+++ Assets/Game resources/Scripts/UI/CanvasSafeAreaHelper.cs
@@ -2,49 +2,49 @@ using UnityEngine;
 
 public class CanvasSafeAreaHelper : MonoBehaviour
 {
-        [SerializeField]  private RectTransform _safeAreaTransform;
-        private Canvas _canvas;
-        private Rect _lastBannerAdjustedSafeArea;
+    [SerializeField]  private RectTransform _safeAreaTransform;
+    
+    private Canvas _canvas;
+    private Rect _lastBannerAdjustedSafeArea;
 
-        private void Awake()
-        {
-            _canvas = GetComponent<Canvas>();
+    private void Awake()
+    {
+        _canvas = GetComponent<Canvas>();
+        UpdateSafeArea();
+    }
 
-            UpdateSafeArea();
-        }
+    private void UpdateSafeArea()
+    {
+        var bannerAdjustedSafeArea = GetBannerAdjustedSafeArea();
 
-        private void UpdateSafeArea()
-        {
-            var bannerAdjustedSafeArea = GetBannerAdjustedSafeArea();
+        if (bannerAdjustedSafeArea == _lastBannerAdjustedSafeArea)
+            return;
 
-            if (bannerAdjustedSafeArea == _lastBannerAdjustedSafeArea)
-                return;
+        _lastBannerAdjustedSafeArea = bannerAdjustedSafeArea;
+        SetSafeAreaRectTransform(_lastBannerAdjustedSafeArea);
+    }
 
-            _lastBannerAdjustedSafeArea = bannerAdjustedSafeArea;
-            SetSafeAreaRectTransform(_lastBannerAdjustedSafeArea);
-        }
-
-        private Rect GetBannerAdjustedSafeArea()
-        {
-            var safeArea = Screen.safeArea;
-            return safeArea;
-        }
+    private Rect GetBannerAdjustedSafeArea()
+    {
+        var safeArea = Screen.safeArea;
+        return safeArea;
+    }
         
-        private void SetSafeAreaRectTransform(Rect newRect)
-        {
-            if (! _safeAreaTransform)
-                return;
-            
-            var anchorMin = newRect.position;
-            var anchorMax = newRect.position + newRect.size;
-            
-            var canvasPixelRect = _canvas.pixelRect;
-            anchorMin.x /= canvasPixelRect.width;
-            anchorMin.y /= canvasPixelRect.height;
-            anchorMax.x /= canvasPixelRect.width;
-            anchorMax.y /= canvasPixelRect.height;
+    private void SetSafeAreaRectTransform(Rect newRect)
+    {
+        if (! _safeAreaTransform)
+            return;
+        
+        var anchorMin = newRect.position;
+        var anchorMax = newRect.position + newRect.size;
+        
+        var canvasPixelRect = _canvas.pixelRect;
+        anchorMin.x /= canvasPixelRect.width;
+        anchorMin.y /= canvasPixelRect.height;
+        anchorMax.x /= canvasPixelRect.width;
+        anchorMax.y /= canvasPixelRect.height;
 
-            _safeAreaTransform.anchorMin = anchorMin;
-            _safeAreaTransform.anchorMax = anchorMax;
-        }
+        _safeAreaTransform.anchorMin = anchorMin;
+        _safeAreaTransform.anchorMax = anchorMax;
+    }
 }
