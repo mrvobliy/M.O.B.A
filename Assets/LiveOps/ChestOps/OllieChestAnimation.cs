bdefedecce7342b3dd6baa0b9a5fbfb8896f39fc
Daniil Gorin <daniil009.gorin@yandex.ru>
2025-02-06 20:52:28 +0300

    add new effects

--- Assets/LiveOps/ChestOps/OllieChestAnimation.cs
+++ Assets/LiveOps/ChestOps/OllieChestAnimation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using AssetKits.ParticleImage;
 using DG.Tweening;
 using Sirenix.OdinInspector;
 using TMPro;
@@ -10,7 +11,8 @@ using UnityEngine.UI;
 public class OllieChestAnimation : MonoBehaviour
 {
     [SerializeField] private Transform _closeChestIcon;
-    [SerializeField] private Transform _openChestIcon;
+    //[SerializeField] private Transform _openChestIcon;
+    [SerializeField] private Transform _chestCover;
     [SerializeField] private CanvasGroup _canvasGroup;
     [SerializeField] private Image _shine;
     [SerializeField] private TMP_Text _openChestButtonText;
@@ -25,6 +27,9 @@ public class OllieChestAnimation : MonoBehaviour
     [SerializeField] private List<Transform> _rows;
     [SerializeField] private List<Transform> _points;
     [SerializeField] private float _timeShowRewardView;
+    [Space] 
+    [SerializeField] private ParticleImage _openEffect;
+    [SerializeField] private ParticleImage _lightLineEffect;
     
     private List<OllieChestRewardView> _rewardsViews = new();
 
@@ -59,12 +64,17 @@ public class OllieChestAnimation : MonoBehaviour
     public void OpenChest()
     {
         var seq = DOTween.Sequence();
-        seq.Append(_closeChestIcon.DOScale(0, 0.3f));
+        seq.Append(_closeChestIcon.DOLocalMove(new Vector3(0, -900, 0), 0.3f));
+        
         seq.Join(_openChestButtonText.DOFade(0, 0.2f));
         seq.JoinCallback(() => {_openChestButton.gameObject.SetActive(false);});
-        seq.Join(_closeChestIcon.DOLocalMove(new Vector3(0, -900, 0), 0.3f));
-        seq.Append(_openChestIcon.DOScale(0.75f, 0.25f));
-        seq.Append(_openChestIcon.DOScale(0.7f, 0.25f));
+        
+        seq.Join(_closeChestIcon.DOScale(new Vector3(0.7f, 0.45f, 0.7f), 0.3f));
+        seq.Append(_closeChestIcon.DOScale(new Vector3(0.7f, 0.8f, 0.7f), 0.3f));
+        seq.Append(_chestCover.DOMove(new Vector3(-16, 5100, 0), 0.7f));
+        seq.Join(_closeChestIcon.DOScale(new Vector3(0.7f, 0.7f, 0.7f), 0.3f));
+        seq.JoinCallback(() => { _lightLineEffect.Play();});
+        seq.JoinCallback(() => {_openEffect.Play();});
         seq.AppendCallback(() =>
         {
             var countRows = _rewardsViews.Count / 4;
@@ -96,16 +106,19 @@ public class OllieChestAnimation : MonoBehaviour
                     seqRewardsViews.Join(rewardView.transform.DOScale(1, _timeShowRewardView));
                     seqRewardsViews.Append(rewardView.Text.DOFade(1, _timeShowRewardView));
                     
-                    _openChestIcon.DOScale(0.65f, _timeShowRewardView / 2).OnComplete(() => { 
-                        _openChestIcon.DOScale(0.7f, _timeShowRewardView / 2);});
+                    _closeChestIcon.DOScale(0.65f, _timeShowRewardView / 2).OnComplete(() => { 
+                        _closeChestIcon.DOScale(0.7f, _timeShowRewardView / 2);});
                     
                     yield return waitTime;
                 }
                 
                 var seq = DOTween.Sequence();
-                seq.Append(_openChestIcon.DOScale(0, _timeShowRewardView));
                 seq.Append(_takeChestButtonText.DOFade(0.16f, 0.3f));
                 seq.AppendCallback(() => {_takeChestButton.gameObject.SetActive(true);});
+                
+                var seq2 = DOTween.Sequence();
+                seq2.Append(_closeChestIcon.DOScale(0.73f, 0.2f));
+                seq2.Append(_closeChestIcon.DOScale(0, 0.2f));
             }
         });
     }
--- Assets/LiveOps/ChestOps/OllieChestRewardView.cs
+++ Assets/LiveOps/ChestOps/OllieChestRewardView.cs
@@ -18,7 +18,7 @@ public class OllieChestRewardView : MonoBehaviour
 		_text.text = reward.Title;
 		_image.sprite = reward.Sprite;
 		_image.transform.localScale = reward.Scale * Vector3.one;
-		transform.localScale = Vector3.zero;
+		transform.localScale = new Vector3(1, 1, 1);
 		transform.localPosition = Vector3.zero;
 	}
 }
