8cf33a4b183e229f54094eab98f83ba9a49f9304
Daniil Gorin <daniil009.gorin@yandex.ru>
2025-02-11 17:58:41 +0300

    delete unneeded files

--- Assets/LiveOps/ChestOps/Scripts/ShineRotate.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class ShineRotate : MonoBehaviour
-{
-    [SerializeField] private float rotationSpeed = 100f;
-
-    private void Update() => transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
-}
